01813e34f40bfb8e78086cb593045ac9fd9356cc
Fateh Aliverdiev <tu7l7qsfn@code.edu.az>
2022-12-13 13:46:36 +0400

    weapontask

---
+++ Weapon task/Models/Weapon.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weapon_task.Models
+{
+    internal class Weapon
+    {
+        string _weaponName;
+        int _bulletCapacity;
+        int _bulletCount;
+        public bool AutoMode;
+        int _bullettime;
+        int onebullettime;
+        public string WeaponName
+        {
+            get { return _weaponName; }
+            set 
+            {
+                if (value == "" || value==null)
+                {
+                    throw new Exception("Null ola bilmez");
+                }
+                else
+                {
+                    _weaponName = value;
+                }
+            }
+        }
+
+        public int BulletCapacity
+        {
+            get { return _bulletCapacity; }
+            set 
+            {
+                if (value > 0)
+                {
+                    _bulletCapacity = value;
+                }
+                else
+                {
+                    throw new Exception("Duzgun gulle tutumu daxil edin");
+                }
+            }
+        }
+        public int BulletCount
+        {
+            get { return _bulletCount; }
+            set 
+            { 
+                if(value>=0 && BulletCapacity >= value)
+                {
+                    _bulletCount = value; 
+                }
+                else
+                {
+                    throw new Exception("Duzgun gulle sayi daxil edin");
+                }
+            }
+        }
+        public int BulletTime
+        {
+            get { return _bullettime; }
+            set
+            {
+                if (value > 0)
+                {
+                    _bullettime = value;
+                }
+                else
+                {
+                    throw new Exception("Duzgun gulle tutumu daxil edin");
+                }
+            }
+        }
+        public Weapon(string weaponname, int bulletcapacity, int bulletcount, int bullettime, bool automode) 
+        {
+            WeaponName = weaponname;
+            BulletCapacity = bulletcapacity;
+            BulletCount = bulletcount;
+            BulletTime= bullettime;
+            AutoMode = automode;
+            onebullettime = bulletcapacity / bullettime;
+            
+        }
+        public void Shoot()
+        {
+            if (BulletCount > 0)
+            {
+                BulletCount--;
+                Console.WriteLine("Bir gulle atildi " + "qalan gulle : " + BulletCount);
+            }
+            else
+            {
+                Console.WriteLine("Atilacaq gulle yoxdu,gulle sayi : " + BulletCount);
+            }
+        }
+        public void Fire()
+        {
+            if (AutoMode == false)
+            {
+                Shoot();
+            }
+            else if(AutoMode == true)
+            {
+                if(BulletCount > 0)
+                {
+                    int remainingbullettime = BulletCount * onebullettime;
+                    Console.WriteLine("butun gulleler ateshlendi , Ateshlenme vaxti : " + remainingbullettime);
+                    BulletCount = 0;
+                }
+                else
+                {
+                    Console.WriteLine("Gulle yoxdu : " + BulletCount);
+                    wrongcommand:
+                    Console.WriteLine("Gulle bitdi gore , reload elemek isteyirsiniz?(he/yox)");
+                    string answer = Console.ReadLine();
+                    if (answer.ToLower().Trim() == "he")
+                    {
+                        Reload();
+                    }
+                    else if (answer.ToLower().Trim()=="yox")
+                    {
+                        Console.WriteLine("Reload olunmadi");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Duzgun command daxil ele!!");
+                        goto wrongcommand;
+                    }
+                }
+            }
+        }
+        public void ChangeFireMod()
+        {
+            if(AutoMode == false)
+            {
+                AutoMode = true;
+            }
+            else
+            {
+                AutoMode= false;
+            }
+            Console.WriteLine("Atish modu deyishdirildi");
+        }
+        public void GetRemainCount()
+        {
+            int remaincount = BulletCapacity - BulletCount;
+            Console.WriteLine("Daragin tam dolmasi ucun lazim olan gulle sayi : " + remaincount);
+        }
+        public void Reload()
+        {
+            BulletCount = BulletCapacity;
+            Console.WriteLine("Gulleniz reload olundu , hal hazirda olan gulle sayi " + BulletCount);
+        }
+    }
+}
---
+++ Weapon task/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel.Design;
+using Weapon_task.Models;
+
+namespace Weapon_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Silah yaratmaq uchun silahin infolarini daxil edin. \nIlk once adini daxil edin : " );
+            string weaponname=Console.ReadLine();
+            Console.WriteLine("Silahin gulle tutumunu daxil edin : ");
+            int bulletcapacity = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Silahin hal hazirki gulle sayini daxil edin : ");
+            int bulletcount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Silahin daraginin boshalma saniyesini daxil edin : ");
+            int bullettime = Convert.ToInt32(Console.ReadLine());
+            Weapon weapon = new Weapon(weaponname , bulletcapacity, bulletcount, bullettime, true);
+            bool end = true;
+            while (end)
+            {
+                Menu:
+                Console.WriteLine("0. Informasiya almaq uchun \n1.Shoot metodunu ishe salmaq uchun \n2.Fire metodunu ishe salmaq uchun \n3.GetRemainCount metodunu isledir \n4.Reload metodu uchun \n5.ChangeFireMode metodu uchun \n6.Exit");
+                int command = Convert.ToInt32(Console.ReadLine());
+                switch (command)
+                {
+                    case 0:
+                        goto Menu;
+                        break;
+                    case 1:
+                        Console.Clear();
+                        weapon.Shoot();
+                        break;
+                    case 2:
+                        Console.Clear();
+
+                        weapon.Fire();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        weapon.GetRemainCount();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        weapon.Reload();
+                        break;
+                    case 5:
+                        Console.Clear();
+                        weapon.ChangeFireMod();
+                        break;
+                    case 6:
+                        Console.Clear();
+                        Console.WriteLine("Proqram dayandi");
+                        end = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Wrong command daxil elediniz!!!");
+                        goto Menu;
+                        break;
+                }
+            }
+         }
+    }
+}
